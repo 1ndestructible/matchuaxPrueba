5003c30a35c9679e1e350d81aeca53bdbcd8d4f8
sergio <sergiogocu2004@gmail.com>
2023-11-06 20:02:54 +0100

    Mensaje ejemplo

---
+++ app/src/main/java/com/example/matchuax/conversaciones.java
@@ -0,0 +1,14 @@
+package com.example.matchuax;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class conversaciones extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conversaciones);
+    }
+}
